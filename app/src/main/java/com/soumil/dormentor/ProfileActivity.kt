58da91047c4ecaab12b6275383666fe7d8e1fbba
Soumil <93309655+soumilj94@users.noreply.github.com>
2023-05-01 00:17:19 +0530

    update

--- app/src/main/java/com/soumil/dormentor/ProfileActivity.kt
+++ app/src/main/java/com/soumil/dormentor/ProfileActivity.kt
@@ -1,12 +1,26 @@
 package com.soumil.dormentor
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Vibrator
+import android.widget.ImageView
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val goBackBtn = findViewById<ImageView>(R.id.goBackMainButton)
+        goBackBtn.setOnClickListener{
+            val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            vibrator.vibrate(20)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+                super.finish()
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
     }
 
     override fun finish() {
