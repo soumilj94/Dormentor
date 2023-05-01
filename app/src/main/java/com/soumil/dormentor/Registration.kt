b4353d4be49e44b8f5bdbd4cef90072333b85e8d
Soumil <93309655+soumilj94@users.noreply.github.com>
2023-05-01 15:25:38 +0530

    update

--- app/src/main/java/com/soumil/dormentor/Registration.kt
+++ app/src/main/java/com/soumil/dormentor/Registration.kt
@@ -2,13 +2,17 @@ package com.soumil.dormentor
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Vibrator
+import android.util.Log
+import android.util.Patterns
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -19,6 +23,9 @@ import com.google.firebase.ktx.Firebase
 class Registration : AppCompatActivity() {
     private val databaseReference = FirebaseDatabase.getInstance("https://dormentor-soumil-default-rtdb.firebaseio.com/").reference
     private lateinit var auth: FirebaseAuth
+    private val sharedPreferences: SharedPreferences by lazy {
+        getSharedPreferences("LOGIN_PREFS", MODE_PRIVATE)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,52 +58,38 @@ class Registration : AppCompatActivity() {
                 Toast.makeText(baseContext, "Email or Password field Empty!", Toast.LENGTH_SHORT).show();
             }
             else{
-//                databaseReference.child("users").addListenerForSingleValueEvent(object: ValueEventListener{
-//                    override fun onDataChange(snapshot: DataSnapshot) {
-////                        check if credentials are present in rtdb
-//                        if (snapshot.hasChild(savedEmail)){
-//                            val getPassword = snapshot.child(savedEmail).child("password").getValue(String::class.java)
-//                                if (getPassword.equals(savedPassword)){
-                                    Toast.makeText(baseContext, "Authentication Successful!", Toast.LENGTH_SHORT).show();
-                                    val i = Intent(baseContext, MainActivity::class.java)
-                                    startActivity(i)
-//                                }
-//                                else{
-//                                    Toast.makeText(baseContext, "Authentication failed!", Toast.LENGTH_SHORT).show()
-//                                }
-//                        }
-//                        else{
-//                            Toast.makeText(baseContext, "Wrong Credentials!", Toast.LENGTH_SHORT).show()
-//                        }
-//                    }
-//
-//                    override fun onCancelled(error: DatabaseError) {
-//                        TODO("Not yet implemented")
-//                    }
-//                })
+                auth.signInWithEmailAndPassword(savedEmail, savedPassword)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithEmail:success")
+                            val user = auth.currentUser
+//                            updateUI(user)
+                            val mainIntent = Intent(this, MainActivity::class.java)
+                            mainIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(mainIntent)
+                            Toast.makeText(baseContext, "Authentication Successfull", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                baseContext,
+                                "Authentication failed.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
+                    }
             }
 
 
 
-//            savedEmail = emailAdd.text.toString().trim()
-//            savedPassword = password.text.toString().trim()
-
-//            auth.signInWithEmailAndPassword(savedEmail, savedPassword)
-//                .addOnCompleteListener(this) { task ->
-//                    if (task.isSuccessful) {
-//                        // Sign in success, update UI with the signed-in user's information
-//                        Log.d("TAG", "signInWithEmail:success")
-//                        val user = auth.currentUser
-//                        Toast.makeText(baseContext, "Authentication Successful!", Toast.LENGTH_SHORT,).show()
-//                        updateUI(user)
-//                    } else {
-//                        // If sign in fails, display a message to the user.
-//                        Log.w("TAG", "signInWithEmail:failure", task.exception)
-//                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT,).show()
-//                        updateUI(null)
-//                    }
-//                }
+//
         }
-//finish()
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        val i = Intent(this, MainActivity::class.java)
+        startActivity(i)
+
     }
 }
